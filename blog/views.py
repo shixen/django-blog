4ab9b0546c9c9e707e51ceaf2f4ab5989276929e
niklas eriksson <niklaseriksson32@hotmail.com>
2024-05-12 09:35:06 +0000

    add forms

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -10,23 +11,13 @@ class PostList(generic.ListView):
 
 
 def post_detail(request, slug):
-    """
-    Display an individual :model:`blog.Post`.
-
-    **Context**
-
-    ``post``
-        An instance of :model:`blog.Post`.
-
-    **Template:**
-
-    :template:`blog/post_detail.html`
-    """
+ 
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    comment_form = CommentForm()
 
     
     return render(
@@ -36,5 +27,6 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         },
     )
